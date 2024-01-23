f154d67b7cdbaaed90e99be13712932bc4168211
DanMark <daniel.mark.kiss@gmail.com>
2024-01-23 22:14:55 +0100

    feat: :sparkles: this is not working

--- version_3.0/stream_to_server/server.js
+++ version_3.0/stream_to_server/server.js
@@ -8,16 +8,24 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const PORT = process.env.PORT || 3000;
+let clients = new Set();
 
 app.use(express.static("public"));
 
 wss.on("connection", (socket) => {
   console.log("Client connected");
+  clients.add(socket);
 
   socket.on("message", (message) => {
     // Assuming the message is a chunk of video data
     // You can save the chunks, process them, or broadcast to other clients
     console.log("Received chunk:", message.length);
+    for (const client of clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        console.log("Sending chunk to client");
+        client.send(message);
+      }
+    }
   });
 
   socket.on("close", () => {
