ae1b96274802416bd387b7a31fd55552c65e6c31
DanMark <daniel.mark.kiss@gmail.com>
2024-01-27 18:32:06 +0100

    feat: :sparkles: it is working on device!!!

--- tvOS/watchY/watchY/ContentView.swift
+++ tvOS/watchY/watchY/ContentView.swift
@@ -1,26 +1,48 @@
-import SwiftUI
-import AVKit
+//
+//  ContentView.swift
+//  Stream.it tvOS
+//
+//  Created by Mark Howard on 27/09/2021.
+//
 
+import SwiftUI
 
 struct ContentView: View {
-
-    @State var player = AVPlayer(url: (URL(string:"http://localhost:3001/movie")!))
-    @State var isPlaying: Bool = false
-    
+    @State var tabSelection = 1
     var body: some View {
-        VStack {
-            VideoPlayer(player: player)
-                .frame(width: 320, height: 180, alignment: .center)
-
-
-            Button {
-                isPlaying ? player.pause() : player.play()
-                isPlaying.toggle()
-                player.seek(to: .zero)
-            } label: {
-                Image(systemName: isPlaying ? "stop" : "play")
-                    .padding()
-            }
+        NavigationView {
+        TabView(selection: $tabSelection) {
+         Video()
+                .tag(1)
+                .tabItem {
+                    VStack {
+                    Image(systemName: "camera")
+                    Text("Video")
+                    }
+                }
+            Audio()
+                .tag(2)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "headphones")
+                        Text("Audio")
+                    }
+                }
+            SettingsView()
+                .tag(3)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "gearshape")
+                        Text("Settings")
+                    }
+                }
         }
+    }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
