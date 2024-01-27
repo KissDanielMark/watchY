269984622bc9b9ced61f695e59cf95514171e6a6
DanMark <daniel.mark.kiss@gmail.com>
2024-01-27 15:33:16 +0100

    feat: :tada: started

---
+++ tvOS/watchY/watchY/watchYApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchYApp.swift
+//  watchY
+//
+//  Created by Kiss Dániel Márk on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct watchYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
